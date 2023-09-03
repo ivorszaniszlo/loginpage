362e551a65b2725ef0397e14bae2163949a99a6c
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-09-03 18:49:54 +0200

    ADD: context dynamic

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
       <AuthContext.Provider
        value={{
         isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler
        }}
       >
         <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
